cc9e9a4f49d287faf4f5b8eb564fda7e2740f6b5
AbhayVAshokan <abhayvashokan@gmail.com>
2020-03-23 16:11:46 +0530

    messenger app first commit

---
+++ messenger_app/linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "messenger_app";
+const unsigned int kFlutterWindowWidth = 800;
+const unsigned int kFlutterWindowHeight = 600;
